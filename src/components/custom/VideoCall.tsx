00c1db1de87e5420bc440a3205f058f79910cb7b
Riccardo Scaringella <rikirandom9@gmail.com>
2025-09-09 10:09:57 +0200

    - Aggiunta uscita uscita automatica dalla stanza quando streamer esce - Aggiunto controllo esistenza stanza prima di entrare - Aggiunto campo errore - Aggiunta scomparsa pulsante chiusura chiamata a mouse fermo - Aggiunta entrata in chiamata con pulsante invio della tastiera dopo digitazione nome stanza - Pulizia codice

--- src/components/custom/VideoCall.tsx
+++ src/components/custom/VideoCall.tsx
@@ -1,58 +1,91 @@
 import {
-  LocalUser,
   RemoteUser,
   useIsConnected,
   useJoin,
-  useLocalMicrophoneTrack,
-  useLocalCameraTrack,
-  usePublish,
   useRemoteUsers,
   VideoPlayerConfig,
 } from "agora-rtc-react";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export function VideoCall() {
   const [calling, setCalling] = useState(false);
   const isConnected = useIsConnected();
   const [appId, setAppId] = useState("d6dc8886fcc44f29847c7e352b080292"); 
-  const [channel, setChannel] = useState("Unity_Channel"); 
+  const [channel, setChannel] = useState(""); 
   const [token, setToken] = useState("");
   const [mouseOver, setMouseOver] = useState(false);
+  const [error, setError] = useState("");
+
+  const [timer1, setTimer1] = useState<any>(null);
+  const [timer2, setTimer2] = useState<any>(null);
 
   useJoin({appid: appId, channel: channel, token: token ? token : null}, calling);
-  //local user
-  const [micOn, setMic] = useState(false);
-  const [cameraOn, setCamera] = useState(false);
-  /*const { localMicrophoneTrack } = useLocalMicrophoneTrack(micOn);
-  const { localCameraTrack } = useLocalCameraTrack(cameraOn);
-  usePublish([localMicrophoneTrack, localCameraTrack]);*/
+
   //remote users
   const remoteUsers = useRemoteUsers();
+
+  useEffect(() => {
+    if (remoteUsers.filter(user => user.hasVideo).length == 0) {setCalling(false)}
+  }, [remoteUsers]);
+
+  useEffect(() => {
+    if (!error) return;
+    
+    clearTimeout(timer2)
+    setTimer2(setTimeout(() => {
+      setError("")
+    }, 10000));
+
+  }, [error])
 
   const fetchToken = async () => {
     const API_BASE_URL = 'http://localhost:53001';
 
     try {
       const instance = axios.create({
-      baseURL: API_BASE_URL,
-      timeout: 1000,
-      headers: { 'ngrok-skip-browser-warning': 'true', 'Access-Control-Allow-Origin': '*' }
-    });
+        baseURL: API_BASE_URL,
+        timeout: 1000,
+        headers: { 
+          'ngrok-skip-browser-warning': 'true', 
+          'Access-Control-Allow-Origin': '*',
+          'reject-if-empty-channel': 'true'
+        }
+      });
       const request = await instance.get(`/token?channelName=${channel}`)
+      if (request.status == 404) {
+        setError("Channel not found. \n Streamer not connected yet or wrong channel name")
+        return Promise.resolve(false);
+      }
       setToken(request.data.token);
+      return Promise.resolve(true);
+      
     } catch (error) {
-    console.error("Error fetching token:", error);
+      if (error.status == 404) {
+        setError("Channel not found.\nStreamer not connected yet or wrong channel name")
+        return Promise.resolve(false);
+      }
+      console.error("Error fetching token:", error);
+      setError("Server error. Could not fetch token")
+      return Promise.resolve(false);
     }
   };
 
-  const [j, setJ] = useState<any>(null);
+  const addLineBreak = (str: string) =>
+  str.split('\n').map((subStr) => {
+    return (
+      <>
+        {subStr}
+        <br />
+      </>
+    );
+  });
 
   const mouseMove = () => {
-    clearTimeout(j)
+    clearTimeout(timer1)
     setMouseOver(true)
-    setJ(setTimeout(() => {
+    setTimer1(setTimeout(() => {
       setMouseOver(false)
     }, 3000));
   }
@@ -63,24 +96,13 @@ export function VideoCall() {
   } as VideoPlayerConfig
 
   return (
-    <div className="flex flex-col w-full h-full py-5 gap-y-3" 
-      onMouseMove={() => mouseMove()} >
+    <div 
+      className="flex flex-col w-full h-full py-5 gap-y-3" 
+      onMouseMove={() => mouseMove()} 
+    >
       <div className="room flex flex-grow flex-col items-center justify-center">
         {isConnected  ? (
-          <div className="user-list w-full flex flex-row flex-wrap h-full justify-center gap-3  items-center">
-            {false && (
-                <div className="user grow min-w-80">
-                    <LocalUser
-                        //audioTrack={localMicrophoneTrack}
-                        //cameraOn={cameraOn}
-                        ///micOn={micOn}
-                        //videoTrack={localCameraTrack}
-                        
-                    >
-                    </LocalUser>
-                </div>
-            )}
-            
+          <div className="user-list w-full flex flex-row flex-wrap h-full justify-center gap-3  items-center">            
             {remoteUsers.filter((user) => user.hasVideo).map((user) => (
               <div className="user grow min-w-80" key={user.uid}>
                 <RemoteUser 
@@ -102,20 +124,27 @@ export function VideoCall() {
                 value={channel}
                 type="text"
                 pattern="(?:0|[1-9]\d*)"
-                inputMode="numeric"
+                inputMode="text"
                 autoComplete="off"
+                onKeyUp={async (e) => {
+                  if (e.key === 'Enter' && appId && channel) {
+                    if (await fetchToken())
+                      setCalling(true)
+                  }
+                }}
               />
 
               <button
                 className={`transition ease-in-out text-xl font-bold px-4 py-2 rounded-lg hover:bg-slate-300 duration-400  join-channel ${!appId || !channel ? "disabled" : ""}`}
                 disabled={!appId || !channel}
                 onClick={async () => {
-                  await fetchToken();
-                  setCalling(true)
+                  if (await fetchToken())
+                    setCalling(true)
                 }}
               >
               <span>Join Channel</span>
             </button>
+            <span className="text-red-500 text-lg text-center">{addLineBreak(error)}</span>
             </div>
           </div>
         )}
